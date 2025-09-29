bbbfa6c37001ffe5c894b0af2844ec497269654f
Liam Plower <G00467316@atu.ie>
2025-09-29 10:04:26 +0100

    Adding new method called add

--- src/main/java/ie/atu/CalcApp.java
+++ src/main/java/ie/atu/CalcApp.java
@@ -1,4 +1,25 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class CalcApp {
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter your first number: ");
+        int firstNumber = scan.nextInt();
+        System.out.println("Please enter your second number: ");
+        int secondNumber = scan.nextInt();
+
+
+        Calculator calc = new Calculator();
+
+        int result = calc.add(firstNumber, secondNumber);
+        System.out.println(" The result is: " + result);
+
+
+
+
+    }
 }
